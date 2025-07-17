2cefaab3c9fb57d08c15f6f424af8168edc32c0f
Anass Hajjaji <ahajjaji@e3r5p6.1337.ma>
2025-07-17 20:55:44 +0100

    handle kick multiple clients md operator

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -37,7 +37,7 @@ class Channel
 		void addAsClient(Client &client);
 		bool Is_OperatorInChannel(Client &client);  
 		bool Is_OperatorInChannel_2(std::string &nickName) ;
-		void RemoveOperator(std::string &nickName, Client &c, bool stop_kick);
+		void RemoveOperator(std::string &nickName, Client &c);
 		void RemoveClient(std::string &nickName);
 		
 
